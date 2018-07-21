59d6c98da56e88381623dbfc8d6d169224b1ca8a
uzair-shehzad <uzair.shehzad.23897@gmail.com>
2018-07-22 03:53:50 +0500

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.port || 3000;
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
@@ -60,4 +61,6 @@ app.get('/bad',(req,res)=>{
         errorMessage: '404 . Not Found '
     });
 });
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port} `)
+});
